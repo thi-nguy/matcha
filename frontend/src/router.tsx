74c679a6f2683a86bdc21a8df619e0d85563c72f
Stan SBN <stanleyserbin@gmail.com>
2023-12-05 12:15:38 +0000

    feat(frontend): setup frontend part of confirm code page #17

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import ConfirmCodePage from './pages/ConfirmCode';
 import LoginPage from './pages/login';
 import SignupPage from './pages/signup';
 
@@ -8,6 +9,10 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App/>,
+  },
+  {
+    path: '/confirm-code',
+    element: <ConfirmCodePage/>
   },
   {
     path: '/login',
