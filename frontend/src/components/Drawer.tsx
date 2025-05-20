0d6ba8c7a58534edcd2bd6d6d8fd6515c4e5e7f4
Thi hong nhung NGUYEN <thi-nguy@f1r5s5.clusters.42paris.fr>
2025-05-20 18:32:55 +0200

    thinguy/front/profile-drawer: finalize responsive navBar and fix position of drawer

--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -4,7 +4,7 @@ interface DrawerProp {
     children: ReactNode;
 }
 export const Drawer = ({ children }: DrawerProp) => {
-    const baseStyle = "bg-white w-80 fixed left-40 top-32 shadow-lg rounded-3xl";
+    const baseStyle = "bg-white w-80 absolute -right-80 top-40 shadow-lg rounded-3xl";
 
     return (
         <div className={baseStyle}>
