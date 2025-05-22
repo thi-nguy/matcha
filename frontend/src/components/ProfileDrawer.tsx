ed1f57819b5265fdb42ef10bdd6e859d3a265fcd
Thi hong nhung NGUYEN <thi-nguy@f1r5s9.clusters.42paris.fr>
2025-05-22 17:42:36 +0200

    thinguy/front/profile-drawer: add hover effect to My likes avatars

--- frontend/src/components/ProfileDrawer.tsx
+++ frontend/src/components/ProfileDrawer.tsx
@@ -3,10 +3,13 @@ import { Button } from './Button'
 import { FaRegEye } from "react-icons/fa";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { PiSignOutBold } from "react-icons/pi";
+import avatar1 from "../assets/avatar1.jpeg";
+import avatar2 from "../assets/avatar2.jpeg";
+import avatar3 from "../assets/avatar3.jpeg";
 
 export const ProfileDrawer = () => {
     const view = 4242424242;
-    const myLikes = [{ photoSrc: "", profileLink: "/profiles" }, { photoSrc: "", profileLink: "/profiles" }, { photoSrc: "", profileLink: "/profiles" }]
+    const myLikes = [{ photoSrc: avatar1, profileLink: "/profile" }, { photoSrc: avatar2, profileLink: "/profile" }, { photoSrc: avatar3, profileLink: "/profile" }]
     return (
         <div className='flex flex-col gap-4'>
             <div className='grid  grid-cols-[1fr_2fr] border-b'>
@@ -22,22 +25,20 @@ export const ProfileDrawer = () => {
                 <div>Who viewed your profile ({view})</div>
                 <MdKeyboardArrowRight />
             </div>
-            <div className='grid grid-cols-[20%_auto_10%] place-items-center border-b pb-4'>
-                <div>
-                    {myLikes.map((oneLike, i) => {
-                        return (
-                            <a href={oneLike.profileLink} className={`absolute z-${i * 10} left-${i + 5}px`}><img src={oneLike.photoSrc} /></a>
-                        )
-                    })}
-                </div>
+            <div className='grid grid-cols-[auto_10%] place-items-center border-b pb-4'>
+                {myLikes.map((oneLike, i) => {
+                    return (
+                        <a href={oneLike.profileLink} className="absolute hover:scale-125 transition-all duration-400" style={{ left: `${(i + 1) * 20}px`, zIndex: myLikes.length - i }}><img className="w-8 h-8 object-cover rounded-lg border-2 border-white " loading="lazy" src={oneLike.photoSrc} alt={`avatar_${i}`} /></a>
+                    )
+                })}
                 <div>My likes</div>
                 <MdKeyboardArrowRight />
             </div>
-            <div className='grid grid-cols-[20%_auto_10%] place-items-center border-b pb-4'>
+            <div className='grid grid-cols-[20%_auto_10%] place-items-center pb-4'>
                 <PiSignOutBold />
                 <div>Sign out</div>
             </div>
 
-        </div>
+        </div >
     )
 }
