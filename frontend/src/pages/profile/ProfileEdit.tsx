316cb51ac12ba49ab19d897e9ea61498d46e77e4
Thi hong nhung NGUYEN <thi-nguy@f1r7s3.clusters.42paris.fr>
2025-05-28 20:33:23 +0200

    thinguy/front/profile_edit_page: add modal add photo

--- frontend/src/pages/profile/ProfileEdit.tsx
+++ frontend/src/pages/profile/ProfileEdit.tsx
@@ -6,8 +6,21 @@ import avatar2 from "../../assets/avatar2.jpeg";
 import wave from "../../assets/layered-waves-haikei.svg";
 import { BiSolidQuoteAltLeft } from "react-icons/bi";
 import { TbListTree } from "react-icons/tb";
+import { ChangeEvent, useState } from "react";
+import { FaPlus } from "react-icons/fa6";
 
 export const ProfileEdit = () => {
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
+  const [photoUrl, setPhotoUrl] = useState<string[]>(Array(4).fill(null));
+
+  const handleSelectFiles = (event: ChangeEvent<HTMLInputElement>) => {
+    const photoSelected = event.target.files?.[0];
+    if (photoSelected) {
+      const url = URL.createObjectURL(photoSelected);
+    }
+
+  }
   return (
     <div>
       {/* Change photo section */}
@@ -27,8 +40,13 @@ export const ProfileEdit = () => {
           <span>My pictures</span>
         </div>
 
-        <div className="grid grid-cols-4 grid-rows-2 gap-4 items-center">
-          <img
+        <div className="h-[33.33vh] grid grid-cols-4 grid-rows-2 gap-4 items-stretch">
+          <div className="col-span-2 row-span-2 flex justify-center items-center rounded-3xl bg-gray-200"><FaPlus /></div>
+          <div className="flex justify-center items-center bg-gray-200 rounded-3xl"><FaPlus /></div>
+          <div className="flex justify-center items-center bg-gray-200 rounded-3xl"><FaPlus /></div>
+          <div className="flex justify-center items-center bg-gray-200 rounded-3xl"><FaPlus /></div>
+          <div className="flex justify-center items-center bg-gray-200 rounded-3xl"><FaPlus /></div>
+          {/* <img
             className="rounded-3xl 
             col-span-2 row-span-2"
             src={avatar1}
@@ -37,7 +55,7 @@ export const ProfileEdit = () => {
           <img className="rounded-3xl" src={avatar2} alt="avatar" />
           <img className="rounded-3xl" src={avatar2} alt="avatar" />
           <img className="rounded-3xl" src={avatar2} alt="avatar" />
-          <img className="rounded-3xl" src={avatar2} alt="avatar" />
+          <img className="rounded-3xl" src={avatar2} alt="avatar" /> */}
         </div>
 
         <div className="flex justify-end gap-4 p-4">
@@ -49,6 +67,15 @@ export const ProfileEdit = () => {
           </Button>
         </div>
       </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="h-[75vh] w-[50vw] flex flex-col justify-center items-center gap-4 bg-red-100 rounded-xl shadow-slate-300 shadow-2xl">
+          <div className="h-[60vh] w-[33vw] bg-gray-100 border-2 border-dashed border-gray-400 flex justify-center items-center rounded-xl">Drap and Drop your photos here</div>
+          <input type="file" onChange={handleSelectFiles} accept="image/*"></input>
+        </div>
+      )
+      }
 
       {/* Description Section */}
       <div>
